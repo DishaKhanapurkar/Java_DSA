326868f1661dd2f1ff2742a73090eb12801bc750
Disha Khanapurkar <86222578+DishaKhanapurkar@users.noreply.github.com>
2022-02-05 14:31:50 +0530

    Add files via upload

---
+++ Sorting/Bubble/bubblemain.java
@@ -0,0 +1,9 @@
+
+public class bubblemain {
+ public static void main(String args[]) {
+	 bubblesort bs =new bubblesort();
+	 int arr[]= {10,5,30,20,15};
+	 bs.bubblesorting(arr);
+	 bs.printarray(arr);
+ }
+}

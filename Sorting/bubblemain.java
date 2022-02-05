87b0532e76be6c80ae3d9295c9c4638849a2af35
Disha Khanapurkar <86222578+DishaKhanapurkar@users.noreply.github.com>
2022-02-05 14:28:21 +0530

    Delete bubblemain.java

--- Sorting/bubblemain.java
+++
@@ -1,9 +0,0 @@
-
-public class bubblemain {
- public static void main(String args[]) {
-	 bubblesort bs =new bubblesort();
-	 int arr[]= {10,5,30,20,15};
-	 bs.bubblesorting(arr);
-	 bs.printarray(arr);
- }
-}
